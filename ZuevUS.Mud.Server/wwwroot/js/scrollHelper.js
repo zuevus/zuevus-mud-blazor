a4361f1a4d2b4f68daa79814cb8b88dfeab32c27
Yuriy Zuev <Yuriy.Zuev@swtecnn.com>
2025-09-04 13:40:32 +0300

    Add init informations

---
+++ ZuevUS.Mud.Server/wwwroot/js/scrollHelper.js
@@ -0,0 +1,7 @@
+﻿window.scrollHelper = {
+    initialize: function (dotNetHelper) {
+        window.addEventListener("scroll", () => {
+            dotNetHelper.invokeMethodAsync("OnScroll", window.scrollY);
+        });
+    }
+};
